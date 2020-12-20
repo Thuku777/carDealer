0499ca885d71a1f5c0b87b4b8a3455a95557a9c4
Thuku777 <thukuelvys@gmail.com>
2020-12-20 06:56:10 +0300

    third commit

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+import datetime
+
+from dealers.models import  Dealer
 
 # Create your models here.
+class Car(models.Model):
+    dealer = models.ForeignKey(Dealer, on_delete=models.DO_NOTHING)
+    brand = models.CharField(max_length=255)
+    CATEGORY = (
+        ('New', 'New'),
+        ('Used', 'Used')
+    )
+    category = models.CharField(max_length=50, choices=CATEGORY)
+    image_main = models.ImageField(upload_to='images')
+    image1 = models.ImageField(upload_to='images', blank=True)
+    image2 = models.ImageField(upload_to='images', blank=True)
+    image3 = models.ImageField(upload_to='images', blank=True)
+    image4 = models.ImageField(upload_to='images', blank=True)
+    image5 = models.ImageField(upload_to='images', blank=True)
+     
+    miles = models.IntegerField(blank=True, null=True)
+    TRANSMISSION = (
+        ('Automatic', 'Automatic'),
+        ('Manual', 'Manual')
+    )
+    transmission = models.CharField(max_length=50, choices=TRANSMISSION)
+    YEAR_CHOICES = [(r,r) for r in range(2000, datetime.date.today().year+1)]
+    year = models.IntegerField(('year'), choices=YEAR_CHOICES, default=datetime.datetime.now().year) 
+    power = models.IntegerField()
+    fuel_tank = models.IntegerField()
+    price = models.IntegerField()
+    description = models.TextField()
+    date_posted = models.DateField()
+
+    def __str__(self):
+        return self.brand
+
+
--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from .models import  Car
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    new_cars = Car.objects.filter(category='New')[:6]
+    used_cars = Car.objects.filter(category='Used')[:6]
+
+    context = {
+        'new_cars': new_cars,
+        'used_cars': used_cars,
+    }
+    return render(request, 'index.html', context)
 
 def about(request):
     return render(request, 'about.html')
